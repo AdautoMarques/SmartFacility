1980fa019a5ccd1ef8d888a09c4170fc08c60f5c
AdautoMarques <adautomarques937@gmail.com>
2024-06-26 23:17:49 -0300

    Criando o componente welcome

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Header from '@/src/components/Header'
 import styles from './Home.module.scss'
+import Welcome from '@/src/components/Welcome'
 
 export default function Home(){
   return(
     <div className={styles.container}>
       <Header />
+      <Welcome />
     </div>
   )
 }
---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,23 @@
+import Button from '../Button';
+import styles from './styles.module.scss'
+
+const Welcome = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <h1>Simplifique a gestão técnica do seu condomínio com o <span>SmartFacility </span></h1>
+        <p>Não espere até que os problemas técnicos no seu condomínio se torne um pesadelo incontrolável.
+           Descubra agora como a Smart Facility pode eliminar suas dores de cabeça. 
+           A solução que você precisa está a apenas um clique de distância. 
+           Conheça nossas Soluções</p>
+
+        <Button title='Conheça nossas Soluções' kind='secundary'/>
+      </div>
+      <div className={styles.imagen}>
+
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
