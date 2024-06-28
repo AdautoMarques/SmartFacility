a2fa58cc51f68f529092930359a99b9cf67e41c7
Adauto Marques <adautomarques937@gmail.com>
2024-06-28 17:56:34 -0300

    ...

--- src/components/Contato/index.jsx
+++ src/components/Contato/index.jsx
@@ -1,9 +1,23 @@
 import styles from './styles.module.scss'
+import { Fa500Px } from "react-icons/fa";
+
 
 const Contato = () => {
   return ( 
     <div className={styles.container}>
+      <div className={styles.text}>
+        <span>Fale conosco</span>
+        <h1>SmartFacility</h1>
+        <div>
+          <p>📍  Rua Antônio Alves, n.°24-27, Bauru/SP</p>
+          <p>☎️  +55 14 3227-5346</p>
+          <p>📧  contato@smartfacility.pro</p>
+        </div>
+      </div>
 
+      <div className={styles.form}>
+
+      </div>
     </div>
    );
 }
