c40155e81b6d96390c11ee705f5af9b4e0591da9
Adauto Marques <adautomarques937@gmail.com>
2024-06-28 09:29:57 -0300

    Componente de Gerenciamento terminado

--- src/components/Management/index.jsx
+++ src/components/Management/index.jsx
@@ -7,41 +7,51 @@ import previsaoImg from '../../../public/previsao.webp';
 import otimizacaoImg from '../../../public/otimizacao.webp';
 
 const Management = () => {
-  return ( 
+  return (
     <div className={styles.container}>
-      <h1>Gerenciamento de Instalações</h1>
-      <p>Simplificando a gestão técnica de condomínios com soluções inteligentes, 
+      <h1 className={styles.principal}>Gerenciamento de Instalações</h1>
+      <p className={styles.pprincipal}>Simplificando a gestão técnica de condomínios com soluções inteligentes,
         a Smart Facility é o parceiro ideal para uma vida sem preocupações.
       </p>
-import ImageManagement from '../../../public/imagem-4.webp'
-      <Image src={ImageManagement} alt='imagem de gerenciamento de rede'/>
-
+      <div className={styles.image}>
+        <Image src={ImageManagement} alt='imagem de gerenciamento de rede' />
+      </div>
       <div className={styles.managements}>
         <div className={styles.management}>
-          <Image src={coletaImg} alt='coleta de dados'/>
+          <div className={styles.img}>
+            <Image src={coletaImg} alt='coleta de dados' />
+          </div>
           <h1>Coleta de Dados</h1>
           <p>Modelagem com inserção de dados da construção do modelo BIM baseado nas informações geométricas</p>
         </div>
         <div className={styles.management}>
-          <Image src={segmenImg} alt='segmentacao'/>
+          <div className={styles.img}>
+            <Image src={segmenImg} alt='segmentacao' />
+          </div>
           <h1>Segmentação</h1>
           <p>Desenvolvimento da parametrização de todos os elementos do empreendimento que serão gerenciados pela operação</p>
         </div>
         <div className={styles.management}>
-          <Image src={previsaoImg} alt='previsão'/>
+          <div className={styles.img}>
+            <Image src={previsaoImg} alt='previsão' />
+          </div>
           <h1>Previsão</h1>
-          <p>Geração de Ordens de Serviços com automatização da Manutenção Preventiva</p>
+          <p>Geração de Ordens de Serviços com automatização da Manutenção Preventiva podendo antecipar as 
+            necessidades orçamentárias, priorização das Ordens de Serviço
+          </p>
         </div>
         <div className={styles.management}>
-          <Image src={otimizacaoImg} alt='otimização'/>
+          <div className={styles.img}>
+            <Image src={otimizacaoImg} alt='otimização' />
+          </div>
           <h1>Otimização</h1>
-          <p>Gerenciamento de ativos com integração da manutenção de ativos, 
+          <p>Gerenciamento de ativos com integração da manutenção de ativos,
             centralização das solicitações de serviços e automatização dos processos de Gestão
           </p>
         </div>
       </div>
     </div>
-   );
+  );
 }
- 
+
 export default Management;
